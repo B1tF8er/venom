e532ed81177b545e7c5ad3ef99da7964a95b6d18
B1tF8er <ofeth@hotmail.com>
2019-01-05 20:51:44 -0700

    Fix spaces

--- Venom/UriExtensions.cs
+++ Venom/UriExtensions.cs
@@ -6,8 +6,10 @@ namespace Venom
     {
         internal static bool IsTourCategory(this Uri uri) =>
             uri.PathAndQuery.Contains("tour");
+            
         internal static bool IsReviewCategory(this Uri uri) =>
             uri.PathAndQuery.Contains("review");
+
         internal static bool IsVideoCategory(this Uri uri) =>
             uri.PathAndQuery.Contains("video") || uri.PathAndQuery.Contains("cine");
     }
