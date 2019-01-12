89d44af1247ea69f72aa751670160457bc02cfb2
B1tF8er <ofeth@hotmail.com>
2019-01-11 23:14:19 -0700

    Refactor article repo

--- Venom/Repositories/ArticleRepository.cs
+++ Venom/Repositories/ArticleRepository.cs
@@ -1,5 +1,6 @@
 namespace Venom
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
 
@@ -7,12 +8,9 @@ namespace Venom
     {
         public async void AddAsync(IEnumerable<Article> articles)
         {
-            using (var context = new Context()) 
+            using (var context = new Context())
             {
-                articles = articles
-                    .Where(article => 
-                        !context.Authors.AsEnumerable().Any(author => author == article.Author)
-                    );
+                articles = articles.Where(ArticleIsNew(context));
 
                 if (articles.Any())
                 {
@@ -21,5 +19,8 @@ namespace Venom
                 }
             }
         }
+
+        private Func<Article, bool> ArticleIsNew(Context context) =>
+            article => !context.Authors.AsEnumerable().Any(author => author == article.Author);
     }
 }
