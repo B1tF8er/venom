8608071382e4640bc1472a3ffd45fab5d94752d2
B1tF8er <ofeth@hotmail.com>
2019-02-02 00:36:32 -0700

    Add exception handler to main method

--- Venom/Symbiote.cs
+++ Venom/Symbiote.cs
@@ -1,10 +1,24 @@
 ﻿namespace Venom
 {
     using Microsoft.Extensions.DependencyInjection;
+    using System;
     
     class Symbiote
     {
         static void Main(string[] args)
+        {
+            try
+            {
+                Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Environment.FailFast("An unexpected error occurred.", ex);
+            }
+        }
+
+        static void Start()
         {
             var serviceProvider = Startup.ConfigureServiceProvider();
             var crawler = serviceProvider.GetService<ICrawler>();
