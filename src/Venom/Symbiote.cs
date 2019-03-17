d43b601db1d76e83f39dfa6d5953e9d2055f1d08
B1tF8er <ofeth@hotmail.com>
2019-03-17 05:38:43 -0700

    Change project structure

---
+++ src/Venom/Symbiote.cs
@@ -0,0 +1,19 @@
+﻿namespace Venom
+{
+    using System;
+
+    class Symbiote
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Startup.Start();
+            }
+            catch (Exception ex)
+            {
+                Environment.FailFast($"{Error.Unexpected}", ex);
+            }
+        }
+    }
+}
